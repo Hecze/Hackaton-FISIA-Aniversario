c8d8e0d0ef2dd1ed8e7893e3ba6cb58d2b036c49
AULA F.DE ING.DE SISTEMAS E INF <AULAFISI@UNMSM.pe>
2023-11-12 19:31:10 -0500

    Mensaje descriptivo de los cambio s

--- src/components/left-panel/chart/Chart.jsx
+++ src/components/left-panel/chart/Chart.jsx
@@ -2,6 +2,42 @@ import React, { useState, useEffect } from "react";
 import "./Chart.css";
 
 export default function Chart({ datos }) {
+  const [nuevosHorarios, setNuevosHorarios] = useState({}); // Estado para almacenar los nuevos horarios por sección
+
+  const horariosOptions = ["Seleccionar horario", "Lunes 8:00-12:00", "Viernes 8:00-12:00", "Miércoles 2:00-6:00", "Agregar horario"];
+  var aulasOptionsAP = [...Array(3).fill().map((_, i) => Array.from({ length: 9 }, (_, j) => `${i + 1}0${j + 1}`))].flat();
+  var aulasOptionsNP = [...Array(3).fill().map((_, i) => Array.from({ length: 9 }, (_, j) => `${i + 1}0${j + 1}`))].flat();
+  var aulasOptionsAP = aulasOptionsAP.flatMap((aula) => [`${aula} AP`]);
+  var aulasOptionsNP = aulasOptionsNP.flatMap((aula) => [`${aula} NP`]);
+  const aulasOptions = aulasOptionsAP.concat(aulasOptionsNP);
+
+  // Función para manejar el cambio en el select de horarios por sección
+  const handleHorarioChange = (seccion, seleccionado) => {
+    if (seleccionado === "Agregar horario") {
+      // Llamar a tu función para agregar horario
+      agregarNuevoHorario(seccion);
+    } else {
+      // Actualizar el estado de los nuevos horarios solo para la sección específica
+      setNuevosHorarios((prevHorarios) => ({
+        ...prevHorarios,
+        [seccion]: seleccionado,
+      }));
+    }
+  };
+
+  // Función para agregar un nuevo horario por sección
+  const agregarNuevoHorario = (seccion) => {
+    // Lógica de tu función para agregar horario
+    // Por ejemplo, podrías abrir un modal para ingresar el nuevo horario
+    console.log(`Llamar a tu función para agregar horario en la sección ${seccion} aquí`);
+
+    // También podrías limpiar la selección del select para esa sección
+    setNuevosHorarios((prevHorarios) => ({
+      ...prevHorarios,
+      [seccion]: "",
+    }));
+  };
+
   return (
     <div className="chart">
       <table>
@@ -22,28 +58,46 @@ export default function Chart({ datos }) {
                   {asignatura.name}
                 </td>
               </tr>
-              
+
               {Object.values(asignatura.secciones).map((seccion, index) => (
                 <tr key={index}>
                   <td>{index + 1}</td>
                   <td>
                     {seccion.horario ? (
-                      seccion.horario
+                      <select
+                        value={nuevosHorarios[seccion] || ""}
+                        onChange={(e) => handleHorarioChange(seccion, e.target.value)}
+                      >
+                        {horariosOptions.map((option, i) => (
+                          <option key={i} value={option}>
+                            {option}
+                          </option>
+                        ))}
+                      </select>
                     ) : (
                       <div className="add-horario" />
                     )}
                   </td>
-                  <td>{seccion.aula}</td>
+                  <td>
+                    <select>
+                      {aulasOptions.map((option, i) => (
+                        <option key={i} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </select>
+                  </td>
                   <td className="delete">
-                    <h1>
-                      x
-                    </h1>
+                    <h1>x</h1>
                   </td>
                 </tr>
-              )) }
-              <td className="add-seccion" ><h2>+</h2></td>
-              <td colSpan="3" className="add-seccion-span"></td>
-              
+              ))}
+              <tr>
+                <td className="add-seccion">
+                  <h2>+</h2>
+                </td>
+                <td colSpan="3" className="add-seccion-span"></td>
+              </tr>
             </React.Fragment>
           ))}
         </tbody>

c8584481ab839114d2c783459448c97be324e018
Hecze <hectorzerrillo85@gmail.com>
2023-11-21 10:05:49 -0500

    poniendo comillas

--- src/libs/mysql.js
+++ src/libs/mysql.js
@@ -2,10 +2,10 @@ import mysql from 'serverless-mysql';
 
 export const conn = mysql({
     config: {
-        host: viaduct.proxy.rlwy.net,
-        user: root,
-        password: a44256fD5gcd3gFdG65bcD14dEGfBDBD,
+        host: "viaduct.proxy.rlwy.net",
+        user: "root",
+        password: "a44256fD5gcd3gFdG65bcD14dEGfBDBD",
         port: 20048,
-        database: railway,
+        database: "railway",
     },
 });
